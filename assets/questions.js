7ad138ff05a564c93c5b38fbdf8a7d5845969176
Cy Howard <cyhoward47@gmail.com>
2022-06-22 22:16:26 -0500

    finished questions, basic html and css

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Which built-in method removes the last element from an array and returns that element?",
+        multiChoice: ["last()", "get()", "pop()", "None of the above."],
+        answer: "pop()"
+    },
+    {
+        title: "Which of the following function of String object returns a string representing the specified object?",
+        multiChoice: ["toLocaleUpperCase()", "toUpperCase()", "toString()", "substring()"],
+        answer: "toString()"
+    },
+    {
+        title: "Which of the following function of Array object creates a new array with the results of calling a provided function on every element in this array?",
+        multiChoice: ["push()", "join()", "pop()", "map()"],
+        answer: "map()"
+    },
+    {
+        title: "Which of the following function of Array object extracts a section of an array and returns a new array?",
+        multiChoice: ["slice()", "reverse()", "shift()", "some()"],
+        answer: "slice()"
+    },
+    {
+        title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+        multiChoice: ["last()", "put()", "push()", "None of the above."],
+        answer: "push()"
+    },
+];
